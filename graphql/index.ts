33c57b31ce84e6338f4c3ed082f0a501db13bdfb
DracoR22 <rdraco039@gmail.com>
2023-07-31 23:47:29 -0500

    11

--- graphql/index.ts
+++ graphql/index.ts
@@ -84,6 +84,66 @@ export const projectsQuery = `
   }
 `;
 
+export const projectsQueryAll = `
+  query getProjects($endcursor: String) {
+    projectSearch(first: 8, after: $endcursor) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          githubUrl
+          description
+          liveSiteUrl
+          id
+          image
+          category
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const projectsQueryWithFilter = `
+  query getProjects($category: String, $endcursor: String) {
+    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          title
+          githubUrl
+          description
+          liveSiteUrl
+          id
+          image
+          category
+          createdBy {
+            id
+            email
+            name
+            avatarUrl
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const getProjectByIdQuery = `
   query GetProjectById($id: ID!) {
     project(by: { id: $id }) {
@@ -141,18 +201,3 @@ export const getProjectsOfUserQuery = `
   }
 `;
 
-export const projectsQueryAll = `
-  query getProjects($endcursor: String) {
-    projectSearch(first: 8, after: $endcursor) {
-     ...
-    }
-  }
-`;
-
-export const projectsQueryWithFilter = `
-  query getProjects($category: String, $endcursor: String) {
-    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
-      ...
-    }
-  }
-`;
